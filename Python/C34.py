c5226f23bc3d525e4ed5c75d8d89fa59d17f00d1
reyllama <rin4616@gmail.com>
2021-07-28 08:58:25 +0900

    bisection

---
+++ Python/C34.py
@@ -0,0 +1,54 @@
+"""
+34. Find First and Last Position of Element in Sorted Array
+
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+"""
+
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        l, r = 0, len(nums)
+        idx = r // 2 # Bisection
+        flag = False # Has encountered target
+        
+        while l < r:
+            if nums[idx] > target:
+                r = idx 
+                idx = (idx-l) // 2
+            elif nums[idx] < target:
+                l = idx + 1
+                idx = idx + (r-idx) // 2
+            else:
+                flag = True # Has just encountered target value
+                break
+        
+        if flag == False:
+            return [-1, -1]
+                
+        res = []
+        
+        for i in range(1, len(nums)+1): # To the left
+            if idx-i<0 or nums[idx-i] != target:
+                res.append(idx-i+1)
+                break
+
+        for i in range(1, len(nums)+1): # To the right
+            if idx+i>=len(nums) or nums[idx+i] != target :
+                res.append(idx+i-1)
+                break
+                
+        return res
+
+"""
+Runtime: 64 ms, faster than 77.96% of Python online submissions for Find First and Last Position of Element in Sorted Array.
+Memory Usage: 14.8 MB, less than 5.95% of Python online submissions for Find First and Last Position of Element in Sorted Array.
+"""
