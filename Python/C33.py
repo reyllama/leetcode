b26d8e73fdc08bea19cf895ef059f24bfc5b369f
reyllama <rin4616@gmail.com>
2021-08-03 10:01:38 +0900

    intended bisection

---
+++ Python/C33.py
@@ -0,0 +1,32 @@
+"""
+33. Search in Rotated Sorted Array
+
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+"""
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        res = -1
+
+        for i, num in enumerate(nums):
+            if num == target:
+                res = i
+
+        return res            
+
+
+"""
+Runtime: 24 ms, faster than 89.87% of Python online submissions for Search in Rotated Sorted Array.
+Memory Usage: 13.8 MB, less than 12.35% of Python online submissions for Search in Rotated Sorted Array.
+"""
