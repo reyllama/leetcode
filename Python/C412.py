5b4ed55f7ea6cdca7d17aa018f587d9f36685df0
reyllama <rin4616@gmail.com>
2021-08-21 17:02:14 +0900

    super easy

---
+++ Python/C412.py
@@ -0,0 +1,33 @@
+"""
+412. Fizz Buzz
+
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i if non of the above conditions are true.
+"""
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        ans = []
+        for i in range(1, n+1):
+            if i % 15 == 0:
+                ans.append("FizzBuzz")
+            elif i % 3 == 0:
+                ans.append("Fizz")
+            elif i % 5 == 0:
+                ans.append("Buzz")
+            else:
+                ans.append(str(i))
+        return ans
+
+"""
+Runtime: 24 ms, faster than 92.21% of Python online submissions for Fizz Buzz.
+Memory Usage: 14.1 MB, less than 89.45% of Python online submissions for Fizz Buzz.
+"""
