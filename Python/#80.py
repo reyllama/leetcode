4620d977869a88da992182eb727fd78eb73c20d8
reyllama <rin4616@gmail.com>
2022-03-18 09:24:23 +0900

    remove duplicates from sorted array 2

--- Python/#80.py
+++ Python/#80.py
@@ -1,3 +1,31 @@
+"""
+80. Remove Duplicates from Sorted Array II
+
+Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+
+If all assertions pass, then your solution will be accepted.
+"""
+
 class Solution(object):
     def removeDuplicates(self, nums):
         """
@@ -27,3 +55,7 @@ class Solution(object):
                     i += 1
         return k
 
+"""
+Runtime: 46 ms, faster than 70.36% of Python online submissions for Remove Duplicates from Sorted Array II.
+Memory Usage: 13.5 MB, less than 56.05% of Python online submissions for Remove Duplicates from Sorted Array II.
+"""
